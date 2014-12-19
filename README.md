bab5bc13325bbbcd99ab04a0d7b31e1cf57fa593
kxxoling <kxxoling@gmail.com>
2014-12-19 10:49:49 +0800

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+## 前后端融合的 web 组件化开发
+
+活动：SegmentFault D-Day
+
+日期：2014/12/18
+
+联系邮箱：
+
+* wangran[at]pe.vc
+* kxxoling[at]Google 的邮箱
---
+++ slide_config.js
@@ -0,0 +1,36 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: '畅想前后端融合的 web 组件化开发',
+    subtitle: 'SegmentFault Frontend D-Day',
+    eventInfo: {
+      title: 'SegmentFault Frontend D-Day',
+      date: '2014/12/18'
+    },
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/ac-logo.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    theme: ['ac-white'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: '王然',
+    company: 'web 工程师<br>天使汇',
+  }/*, {
+    name: 'Second Name',
+    company: 'Job Title, Google',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  }*/]
+};
+
